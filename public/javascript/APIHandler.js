bee3dc46b2c8b35d97db4da47dbfe9d26da418f5
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-09-06 14:16:05 +0200

    done lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,57 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({ baseURL: this.BASE_URL });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api
+      .get("/characters")
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error in getFullList:", error);
+        throw error;
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(characterId) {
+    return this.api
+      .get(`/characters/${characterId}`)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error in getting 1 character:", error);
+        throw error;
+      });
   }
 
-  createOneRegister () {
+  createOneRegister(character) {
+    return this.api
+      .post(`/characters`, character)
 
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error in getting 1 character:", error);
+        throw error;
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(characterId, characterInfo) {
+    return this.api
+      .put(`/characters/${characterId}`, characterInfo)
+      .then((response) => {})
+      .catch((error) => {
+        console.error("Error in deleting 1 character:", error);
+        throw error;
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    return this.api
+      .delete(`/characters/${characterId}`)
+      .then((response) => {})
+      .catch((error) => {
+        console.error("Error in deleting 1 character:", error);
+        throw error;
+      });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,142 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+const characterInfo = document.querySelector(".character-info");
+const characterContainer = document.querySelector(".characters-container");
+const characterName = document.querySelector(".name");
+const characterOccupation = document.querySelector(".occupation");
+const characterCartoon = document.querySelector(".cartoon");
+const characterWeapon = document.querySelector(".weapon");
 
-  });
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI
+        .getFullList()
+        .then((characters) => {
+          // Handle success (e.g., show a success message)
+          characters.forEach((character) => {
+            const characterBox = document.createElement("div");
+            characterBox.classList.add("character-box");
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+            const characterId = document.createElement("div");
+            characterId.innerHTML = ` <b>Character ID:</b>${character.id}`;
 
-  });
+            const characterName = document.createElement("div");
+            characterName.innerHTML = ` <b>Character Name:</b>${character.name}`;
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+            const characterOccupation = document.createElement("div");
+            characterOccupation.innerHTML = ` <b>Character Occupation:</b>${character.occupation}`;
 
-  });
+            const characterCartoon = document.createElement("div");
+            characterCartoon.innerHTML = ` <b>Is a Cartoon?:</b>${character.cartoon}`;
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+            const characterWeapon = document.createElement("div");
+            characterWeapon.innerHTML = ` <b>Character Weapon:</b>${character.weapon}`;
 
-  });
+            characterBox.appendChild(characterName);
+            characterBox.appendChild(characterOccupation);
+            characterBox.appendChild(characterCartoon);
+            characterBox.appendChild(characterWeapon);
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+            characterContainer.appendChild(characterBox);
+          });
+          characterInfo.style.display = "none";
+        })
+        .catch((error) => {
+          // Handle errors (e.g., display an error message)
+          console.error("Error getting character:", error);
+        });
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      const characterId = document.getElementById("fetchOne");
+      charactersAPI
+        .getOneRegister(characterId.value)
+        .then((character) => {
+          characterName.innerHTML = ` <b>Character Name:</b>${character.name}`;
+          characterOccupation.innerHTML = ` <b>Character Occupation:</b>${character.occupation}`;
+          characterCartoon.innerHTML = ` <b>Is a Cartoon?:</b>${character.cartoon}`;
+          characterWeapon.innerHTML = ` <b>Character Weapon:</b>${character.weapon}`;
+        })
+        .catch((error) => {
+          // Handle errors (e.g., display an error message)
+          console.error("Error getting 1 character:", error);
+        });
+    });
+
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      const characterId = document.getElementById("deleteOne");
+      console.log(characterId.value);
+
+      charactersAPI
+        .deleteOneRegister(characterId.value)
+        .then((characters) => {
+          document.getElementById("delete-one").style.backgroundColor = "green";
+        })
+        .catch((error) => {
+          // Handle errors (e.g., display an error message)
+          console.error("Error deleting 1 character:", error);
+          document.getElementById("delete-one").style.backgroundColor = "red";
+        });
+    });
+
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const characterId = document.getElementById("id1").value;
+      const characterName = document.getElementById("name1").value;
+      const characterOccupation = document.getElementById("occupation1").value;
+      const characterCartoon = document.getElementById("cartoon1").checked;
+      const characterWeapon = document.getElementById("weapon1").value;
+      const character = {
+        name: characterName,
+        occupation: characterOccupation,
+        cartoon: characterCartoon,
+        weapon: characterWeapon,
+      };
+      console.log(character);
+      charactersAPI
+        .updateOneRegister(characterId, character)
+        .then((character) => {
+          document.getElementById("send-data1").style.backgroundColor = "green";
+        })
+        .catch((error) => {
+          // Handle errors (e.g., display an error message)
+          console.error("Error deleting 1 character:", error);
+          document.getElementById("send-data1").style.backgroundColor = "red";
+        });
+    });
+
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const characterName = document.getElementById("name").value;
+      const characterOccupation = document.getElementById("occupation").value;
+      const characterCartoon = document.getElementById("cartoon").checked;
+      const characterWeapon = document.getElementById("weapon").value;
+      const character = {
+        name: characterName,
+        occupation: characterOccupation,
+        cartoon: characterCartoon,
+        weapon: characterWeapon,
+      };
+      charactersAPI
+        .createOneRegister(character)
+        .then((character) => {
+          document.getElementById("delete-one").style.backgroundColor = "green";
+        })
+        .catch((error) => {
+          // Handle errors (e.g., display an error message)
+          console.error("Error deleting 1 character:", error);
+          document.getElementById("delete-one").style.backgroundColor = "red";
+        });
+    });
 });
